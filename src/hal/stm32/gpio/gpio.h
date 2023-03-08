13162af9cb65001958e8b18ddbf673b5b7e148bf
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-03-08 15:21:56 +0000

    update

--- src/hal/stm32/gpio/gpio.h
+++ src/hal/stm32/gpio/gpio.h
@@ -2,6 +2,7 @@
 #define HAL_GPIO_H
 
 #include "stdint.h"
+#include <stdbool.h>
 
 typedef enum {
     GPIO_LOW = 0;
@@ -16,14 +17,23 @@ typedef struct {
 } GPIO_CONFIG;
 
 void Gpio_Init(const GPIO_CONFIG* const config);
-GPIO_STATE Gpio_Read(const GPIO_h pin);
-void Gpio_Set(GPIO_h pin, GPIO_STATE state);
-void Gpio_Toggle(GPIO_h pin);
 void Gpio_Mode_Set(GPIO_h pin, GPIO_MODE mode);
 void Gpio_Dir_Set(GPIO_h pin, GPIO_DIR dir);
-void Gpio_Callback_Register(GPIO_h pin, GPIO_CALLBACK function, void (*cb)(void));
+void Gpio_Callback_Register(GPIO_h pin, GPIO_CALLBACK function, void (*cb)(void));  // really need this?
 
-// Use these to extend the API
+GPIO_STATE Gpio_Read(const GPIO_h pin);
+
+void Gpio_Set(GPIO_h pin, GPIO_STATE state);
+void Gpio_Set_Low(GPIO_h pin);
+void Gpio_Set_High(GPIO_h pin);
+void Gpio_Toggle(GPIO_h pin);
+
+bool Gpio_Is_Set_High(GPIO_h pin);  // for output
+bool Gpio_Is_Set_Low(GPIO_h pin);  // for output
+bool Gpio_Is_High(GPIO_h pin);  // for input
+bool Gpio_Is_Low(GPIO_h pin);  // for input
+
+// Use these SPARINGLY to extend the API
 void Gpio_Reg_Write (uintptr address, uint32_t val);
 uint32_t Gpio_Reg_Read (uintptr address);
 
