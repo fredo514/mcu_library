0cfe64ef03c774b2668e73c904fe3a04e4c51cd0
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-03-08 15:25:03 +0000

    update

--- src/hal/stm32/gpio/gpio.h
+++ src/hal/stm32/gpio/gpio.h
@@ -3,6 +3,7 @@
 
 #include "stdint.h"
 #include <stdbool.h>
+#include "error.h"
 
 typedef enum {
     GPIO_LOW = 0;
@@ -16,17 +17,17 @@ typedef struct {
     GPIO_MODE mode;
 } GPIO_CONFIG;
 
-void Gpio_Init(const GPIO_CONFIG* const config);
-void Gpio_Mode_Set(GPIO_h pin, GPIO_MODE mode);
-void Gpio_Dir_Set(GPIO_h pin, GPIO_DIR dir);
-void Gpio_Callback_Register(GPIO_h pin, GPIO_CALLBACK function, void (*cb)(void));  // really need this?
+ERROR_CODE Gpio_Init(const GPIO_CONFIG* const config);
+ERROR_CODE Gpio_Mode_Set(GPIO_h pin, GPIO_MODE mode);
+ERROR_CODE Gpio_Dir_Set(GPIO_h pin, GPIO_DIR dir);
+ERROR_CODE Gpio_Callback_Register(GPIO_h pin, GPIO_CALLBACK function, void (*cb)(void));  // really need this?
 
 GPIO_STATE Gpio_Read(const GPIO_h pin);
 
-void Gpio_Set(GPIO_h pin, GPIO_STATE state);
-void Gpio_Set_Low(GPIO_h pin);
-void Gpio_Set_High(GPIO_h pin);
-void Gpio_Toggle(GPIO_h pin);
+ERROR_CODE Gpio_Set(GPIO_h pin, GPIO_STATE state);
+ERROR_CODE Gpio_Set_Low(GPIO_h pin);
+ERROR_CODE Gpio_Set_High(GPIO_h pin);
+ERROR_CODE Gpio_Toggle(GPIO_h pin);
 
 bool Gpio_Is_Set_High(GPIO_h pin);  // for output
 bool Gpio_Is_Set_Low(GPIO_h pin);  // for output
@@ -34,7 +35,7 @@ bool Gpio_Is_High(GPIO_h pin);  // for input
 bool Gpio_Is_Low(GPIO_h pin);  // for input
 
 // Use these SPARINGLY to extend the API
-void Gpio_Reg_Write (uintptr address, uint32_t val);
+ERROR_CODE Gpio_Reg_Write (uintptr address, uint32_t val);
 uint32_t Gpio_Reg_Read (uintptr address);
 
 #endif // HAL_GPIO_H
