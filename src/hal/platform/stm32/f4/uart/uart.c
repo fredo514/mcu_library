5a541aebbc9c014ef46e39d8072541f1f229e542
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-04-01 00:57:33 +0000

    update

---
+++ src/hal/platform/stm32/f4/uart/uart.c
@@ -0,0 +1,5 @@
+#include "uart.h"
+
+typedef struct {
+    
+} UART_CTX_t;
--- src/hal/uart.h
+++ src/hal/uart.h
@@ -8,11 +8,57 @@
 
 typedef UART_CTX_t * const UART_h;
 
+typedef enum {
+    UART_9K6_BAUD = 9600,
+    UART_57K6_BAUD = 57600,
+    UART_115K2_BAUD = 115200,
+    UART_BAUD_MAX
+} UART_BAUD_RATE_t;
+
+typedef enum {
+    UART_7BIT_WORD,
+    UART_8BIT_WORD,
+    UART_WORD_MAX
+} UART_WORD_LEN_t;
+
+typedef enum {
+    UART_NO_PARITY,
+    UART_ODD_PARITY,
+    UART_EVEN_PARITY,
+    UART_MARK_PARITY,
+    UART_SPACE_PARITY,
+    UART_PARITY_MAX
+} UART_STOP_BITS_t;
+
+typedef enum {
+    UART_1_STOPBIT,
+    UART_2_STOPBIT,
+    UART_STOPBIT_MAX
+} UART_PARITY_t;
+
+typedef enum {
+
+} UART_MODE_t;
+
 typedef struct {
+    UART_BAUD_RATE_t baud_rate;
+    UART_WORD_LEN_t word_length;
+    UART_STOP_BITS_t stop_bits;
+    UART_PARITY_t parity;
+    UART_MODE_t mode;
 } UART_CONFIG_t;
 
+typedef enum {
+    UART_TX_HALFCOMPLETE_CALLBACK,
+    UART_TX_COMPLETE_CALLBACK,
+    UART_RX_HALFCOMPLETE_CALLBACK,
+    UART_RX_COMPLETE_CALLBACK,
+    UART_ERROR_CALLBACK,
+    UART_ABORT_COMPLETE_CALLBACK,
+} UART_CALLBACK_t;
+
 ERROR_CODE_t Uart_Init(UART_h uart, UART_CONFIG_t const * const config);
-ERROR_CODE_t Uart_Callback_Register(UART_h uart, UART_CALLBACK_t const function, void (*cb)(void));  // really need this?
+ERROR_CODE_t Uart_Callback_Register(UART_h uart, UART_CALLBACK_t const function, void (*cb)(UART_h uart));  // really need this?
 
 ERROR_CODE_t Uart_Char_Put(UART_h uart, uint8_t const ch);
 ERROR_CODE_t Uart_Break(UART_h uart);
