2030dbceba407d8f2e3e393441ca1796b4d6b675
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-05-26 19:41:52 +0000

    update

--- src/hal/spi.h
+++ src/hal/spi.h
@@ -9,6 +9,11 @@
 #include "spi_def.h"	// contains platform-specific defines
 #include "gpio.h"
 
+typedef enum {
+    SPI_CALLBACK_XFER_DONE,
+    SPI_CALLBACK_ID_MAX
+} SPI_CALLBACK_ID_t;
+
 typedef SPI_CTX_t * const SPI_h;
 
 typedef struct {
@@ -23,15 +28,12 @@ typedef struct {
 
 SPI_h Spi_Create(SPI_TypeDef const * const instance);
 ERROR_CODE_t Spi_Init(SPI_h spi, SPI_CONFIG_t const * const config);
-ERROR_CODE_t Spi_Callback_Register(SPI_h spi, SPI_CALLBACK_t const function, void (*cb)(void));  // really need this?
-
+ERROR_CODE_t Spi_Callback_Register(SPI_h spi, SPI_CALLBACK_ID_t const function, void (*cb)(void));
+ERROR_CODE_t Spi_Buffer_Attach(SPI_h spi, uint8_t const * const buf);
 ERROR_CODE_t Spi_Mode_Set(SPI_h spi, SPI_MODE_t const mode);
+SPI_STATUS_t Spi_Status_Get(SPI_h spi);
 
-uint8_t Spi_Exchange(SPI_h spi, uint8_t const ch);
-
-// TODO: non-blocking API
-
-bool Spi_Is_Busy(SPI_h spi);
+uint8_t Spi_Exchange(SPI_h spi, uint8_t const * const tx_buf, uint8_t const * const rx_buf, uint8_t len);
 
 // Use these SPARINGLY to extend the API
 ERROR_CODE_t Spi_Reg_Write (REG_SIZE_t * const address, uint32_t const val);
