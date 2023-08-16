e5d7f2c389752b28044e6330fe1d230cc7765708
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-08-16 18:47:15 +0000

    update

--- src/utils/delay/delay.c
+++ src/utils/delay/delay.c
@@ -1,10 +1,10 @@
 #include "delay.h"
 #include "timer.h"
 
-TIMER_h timer;
+static TIMER_h timebase_timer;
 
 ERROR_CODE_t Delay_Init(TIMER_h delay_timer, uint32_t tick_period_us) {
-    timer = delay_timer;
+    timebase_timer = delay_timer;
     
     TIMER_CONFIG_t config = {
         .period_us = tick_period_us,
@@ -14,12 +14,12 @@ ERROR_CODE_t Delay_Init(TIMER_h delay_timer, uint32_t tick_period_us) {
         autoreload = true;
     };
     
-    Timer_Init(timer, &config);
-    Timer_Start(timer);
+    Timer_Init(timebase_timer, &config);
+    Timer_Start(timebase_timer);
 }
 
 uint32_t Delay_Ticks_now(void) {
-    return Timer_Tick_Get(timer);
+    return Timer_Tick_Get(timebase_timer);
 }
 
 uint32_t Delay_Ticks_Since(uint32_t ticks_then) { 
