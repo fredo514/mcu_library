2ad3ae08c3ef63bad942fce82b38950d02649477
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-08-16 18:46:07 +0000

    update

--- src/utils/delay/delay.c
+++ src/utils/delay/delay.c
@@ -10,27 +10,31 @@ ERROR_CODE_t Delay_Init(TIMER_h delay_timer, uint32_t tick_period_us) {
         .period_us = tick_period_us,
         counter_mode = TIMER_UP_COUNT,
         clock_source = TIMER_PLL,
-        clock_mode;
-        autoreload = true
+        clock_mode = ;
+        autoreload = true;
     };
     
     Timer_Init(timer, &config);
     Timer_Start(timer);
 }
 
-uint32_t Delay_Time_Since(uint32_t time_then) { 
-    uint32_t now = Timer_Tick_Get(timer); 
-
-    // Has rollover occurred?
-    if (now <= time_then) { 
-        // rollover has occurred 
-        return (now - time_then); 
-    } 
-
-    return (now + (TIMER_TICK_MAX - time_then)); 
+uint32_t Delay_Ticks_now(void) {
+    return Timer_Tick_Get(timer);
 }
 
-bool Delay_Is_Elapsed_ms(uint32_t time_then, uint32_t period) { 
-    return (Delay_Time_Since(time_then) >= period); 
+uint32_t Delay_Ticks_Since(uint32_t ticks_then) { 
+    uint32_t now = delay_Ticks_now(); 
+
+    // Has rollover occurred?
+    if (now <= ticks_then) { 
+        // rollover has occurred 
+        return (now + (TIMER_TICK_MAX - ticks_then)); 
+    } 
+
+    return (now - ticks_then); 
+}
+
+bool Delay_Is_Elapsed(uint32_t ticks_then, uint32_t duration) { 
+    return (Delay_Ticks_Since(ticks_then) >= duration); 
 } 
 
--- src/utils/delay/delay.h
+++ src/utils/delay/delay.h
@@ -3,9 +3,9 @@
 
 #include "stdint.h"
 #include "stdbool.h"
-#include "core.h"
 
-bool Delay_Is_Elapsed(uint32_t time_then, uint32_t period);
-uint32_t Delay_Time_Since(uint32_t time_then);
+uint32_t Delay_Ticks_now(void);
+bool Delay_Is_Elapsed(uint32_t ticks_then, uint32_t duration);
+uint32_t Delay_Time_Since(uint32_t ticks_then);
 
 #endif DELAY_H
