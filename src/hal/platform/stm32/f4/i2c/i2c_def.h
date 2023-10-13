025bf0502c8647aea898d55feead801a7771842b
fredo514 <48796368+fredo514@users.noreply.github.com>
2023-10-13 12:44:25 +0000

    update

--- src/hal/platform/stm32/f4/i2c/i2c_def.h
+++ src/hal/platform/stm32/f4/i2c/i2c_def.h
@@ -2,7 +2,7 @@
 #define I2C_DEF_H
 
 typedef enum {
-    I2C_SPEED_100KHZ = // TIMINGR register value,
+    I2C_SPEED_100KHZ = // TIMINGR register value, TODO: autocalculate?
     I2C_SPEED_MAX
 } I2C_SPEED_t;
 
