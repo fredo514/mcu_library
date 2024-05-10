5f93edbbd76b3e0a1e10d5a716d35fdd7aaa1534
fredo514 <48796368+fredo514@users.noreply.github.com>
2024-05-10 18:23:39 +0000

    update

--- src/utils/sllist.h
+++ src/utils/sllist.h
@@ -2,13 +2,11 @@
 
 // wrap the node content with a slnode_t into a struct
 
-struct _snode {
+typedef struct _snode {
         struct _snode *next;
-};
-typedef struct _snode slnode_t;
+} slnode_t;
  
-struct _slist {
+typedef struct _slist {
         slnode_t *head;
         slnode_t *tail;
-};
-typedef struct _slist sllist_t;
+} sllist_t;
