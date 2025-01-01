061e8d22c1c4a0359d80eee1eb405743043a79ff
fredo514 <48796368+fredo514@users.noreply.github.com>
2025-01-01 14:00:55 -0500

    update

---
+++ src/drivers/ext_nvm/tests/ext_nvm_tests.c
@@ -0,0 +1,1 @@
+// common tests to all interfaces
--- src/libs/persistent_storage/perst_store.c
+++ src/libs/persistent_storage/perst_store.c
@@ -3,6 +3,7 @@
 // detect errors and default a section to factory/0 if corrupted
 // manage changes/additions to the data in section after a software upgrade
 // manage sections metadata
+// should not corrupt if powered down during write to physical media
 
 // split related data into section
 // section should be oversize to accomodate growth
