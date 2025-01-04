69090d71c7eabbfdf9eaa085e2d8a18f7ac299c4
fredo514 <48796368+fredo514@users.noreply.github.com>
2025-01-03 23:00:23 -0500

    update

--- src/libs/data_model/model_points/pt_uint32.h
+++ src/libs/data_model/model_points/pt_uint32.h
@@ -2,6 +2,7 @@ typedef struct {
     mp_subsciber_uint32_t observer;
     bool is_valid;
     uint32_t value;
+    uint16_t seq_num;
 } pt_uint32_t;
 
 typedef struct {
@@ -9,3 +10,6 @@ typedef struct {
 } pt_uint32_config_t;
 
 error_t pt_uint32_init(pt_uint32_t * const mp, pt_uint32_config_t const* const config);
+
+error_t pt_uint32_read(pt_uint32_t * const mp, uint32_t* const dest, uint16_t* const seq_num);
+uint16_t pt_uint32_write(pt_uint32_t * const mp, uint32_t new_value);
