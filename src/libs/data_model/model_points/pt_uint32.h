984feee0654741399eef4988755ad163fccab55d
fredo514 <48796368+fredo514@users.noreply.github.com>
2025-01-03 23:06:21 -0500

    update

--- src/libs/data_model/model_points/pt_uint32.h
+++ src/libs/data_model/model_points/pt_uint32.h
@@ -1,3 +1,10 @@
+#ifndef PT_UINT32_H
+#define PT_UINT32_H
+
+#include "error.h"
+#include <stdint.h>
+#include <stdbool.h>
+
 typedef struct {
     mp_subsciber_uint32_t observer;
     bool is_valid;
@@ -10,6 +17,16 @@ typedef struct {
 } pt_uint32_config_t;
 
 error_t pt_uint32_init(pt_uint32_t * const mp, pt_uint32_config_t const* const config);
+bool pt_uint32_is_valid(pt_uint32_t const * const mp);
 
+// optionally returns the current sequence number if not null
 error_t pt_uint32_read(pt_uint32_t * const mp, uint32_t* const dest, uint16_t* const seq_num);
+
+// returns the new sequence number
 uint16_t pt_uint32_write(pt_uint32_t * const mp, uint32_t new_value);
+
+// returns the current sequence number
+uint16_t pt_uint32_set_invalid(pt_uint32_t * const mp);
+bool pt_uint32_is_valid(pt_uint32_t const * const mp);
+
+#endif
