0d8971846e79d9e723c4aefe8f0011d38b4a892a
fredo514 <48796368+fredo514@users.noreply.github.com>
2025-01-05 17:05:41 -0500

    update

--- src/utils/mem_pool/mem_pool.h
+++ src/utils/mem_pool/mem_pool.h
@@ -7,7 +7,7 @@
 typedef struct MEM_POOL_CTX * MEM_POOL_h;
 typedef struct MEM_BLOCK_CTX * MEM_BLOCK_h;
 
-MEM_POOL_h mempool_create(uint16_t block_size, uint16_t num_blocks); 
+MEM_POOL_h mempool_create(size_t block_size, size_t num_blocks); 
 MEM_BLOCK_h mempool_alloc(MEM_POOL_h pool); 
 ERROR_CODE_t mempool_free(MEM_POOL_h pool, MEM_BLOCK_h block); 
 
