90e88c96252e38fc8843b8f387b4c3e9c9c98b4b
fredo514 <48796368+fredo514@users.noreply.github.com>
2025-05-01 18:28:32 -0400

    update

---
+++ tools/check_manifest.py
@@ -0,0 +1,9 @@
+ensure platform support
+    using $TARGET_PLATFORM to check support in manifest
+
+ensure depedencies version
+    use git describe --tags to get version
+
+ensure callback inplementation
+    grep for the function declaration in app src
+
